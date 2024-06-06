4430f95c1849287782bbee35df99483a341e32d2
Sai Praneeth <kurapatisaipraneeth@gmail.com>
2024-06-06 16:30:24 -0500

    Initial Commit

---
+++ backend/core/config.py
@@ -0,0 +1,6 @@
+
+class Settings:
+    PROJECT_TITLE = "Blog"
+    PROJECT_VERSION = "0.1.0"
+
+settings = Settings()
---
+++ backend/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from core.config import settings
+
+app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+@app.get("/")
+def hello():
+    return {"msg": "hello world"}
