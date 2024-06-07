5e7edb07f3381633c0689c7c33a64d0f246e516c
Sai Praneeth <kurapatisaipraneeth@gmail.com>
2024-06-07 15:59:23 -0500

    Add Response Model and status code

--- backend/apis/v1/route_user.py
+++ backend/apis/v1/route_user.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from schemas.user import UserCreate
+from schemas.user import UserCreate, ShowUser
 from db.session import get_db
 from db.repository.user import create_new_user
 
 router = APIRouter()
 
-@router.post("/")
+@router.post("/", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreate, db: Session= Depends(get_db)):
     user = create_new_user(user=user, db=db)
     return user
--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel, EmailStr, Field
 class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=4)
+
+#hiding some of the fields
+class ShowUser(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+            orm_mode = True
