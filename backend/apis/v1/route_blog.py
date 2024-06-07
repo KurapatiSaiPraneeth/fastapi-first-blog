cb095e851c0e6b27f3845219180b76bf7d302b26
Sai Praneeth <kurapatisaipraneeth@gmail.com>
2024-06-07 16:45:36 -0500

    Retreive All Blogs

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,8 +1,9 @@
+from typing import List
 from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.orm import Session
 from db.session import get_db
 from schemas.blog import CreateBlog, ShowBlog
-from db.repository.blog import create_new_blog, retreive_blog
+from db.repository.blog import create_new_blog, retreive_blog, list_blogs
 
 
 router = APIRouter()
@@ -19,5 +20,11 @@ def get_blog(id: int, db: Session = Depends(get_db)):
     if not blog:
         raise HTTPException(detail=f"Blog with id {id} does not exist", status_code=status.HTTP_404_NOT_FOUND)
     return blog
+
+@router.get("", response_model=List[ShowBlog])
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = list_blogs(db=db)
+    return blogs
+
 
 
